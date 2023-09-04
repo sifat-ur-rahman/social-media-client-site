01ad27d46ff07d33b0576b0a9c939f692203fa80
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-09-04 11:57:10 +0600

    add

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { AuthContext } from '../Contexts/AuthProvider';
 const Navbar = () => {
 
   const {user, logOut} = useContext(AuthContext)
-  console.log(user)
+  // console.log(user)
     const handleLogOut = ()=>{
       logOut()
       .then(()=>{})
